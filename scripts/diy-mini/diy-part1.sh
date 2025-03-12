49b477320c1916cc76baca1e0fa20ab448e3dc62
qiaodapei <qiaodapei@hotmail.com>
2025-03-12 21:07:08 +0800

    Update diy-part1.sh

--- scripts/diy-mini/diy-part1.sh
+++ scripts/diy-mini/diy-part1.sh
@@ -9,5 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-sed -i '1i src-git linkease_nas https://github.com/linkease/nas-packages.git' feeds.conf.default
-sed -i '2i src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '3i src-git linkease_nas https://github.com/linkease/nas-packages.git' feeds.conf.default
+sed -i '4i src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci.git' feeds.conf.default
