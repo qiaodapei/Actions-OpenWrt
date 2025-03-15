e91ca955957d5643720eaa77ff7c0a83ee11dca2
qiaodapei <qiaodapei@hotmail.com>
2025-03-16 00:10:59 +0800

    Update diy-part2.sh

--- scripts/diy-mini/diy-part2.sh
+++ scripts/diy-mini/diy-part2.sh
@@ -54,6 +54,9 @@ cp -rf ../../kiddin9/luci-app-netdata/* luci-app-netdata
 mkdir luci-app-rtbwmon
 cp -rf ../../kiddin9/luci-app-rtbwmon/* luci-app-rtbwmon
 
+# mkdir luci-app-guest-wifi
+# cp -rf ../../kiddin9/luci-app-guest-wifi/* luci-app-guest-wifi
+
 # poweroff关机插件
 git clone https://github.com/DongyangHu/luci-app-poweroff.git
 
@@ -61,22 +64,46 @@ popd
 
 # 添加第三方应用
 echo "
+###################################################
 # 系统相关应用
+###################################################
 CONFIG_PACKAGE_luci-app-poweroff=y
-CONFIG_PACKAGE_luci-app-fileassistant=y
 # CONFIG_PACKAGE_luci-app-guest-wifi=y
-CONFIG_PACKAGE_luci-app-onliner=y
-CONFIG_PACKAGE_luci-app-eqos=y
 # CONFIG_PACKAGE_luci-app-wolplus=y
-CONFIG_PACKAGE_luci-app-wifischedule=y
-CONFIG_PACKAGE_luci-app-ramfree=y
 # CONFIG_PACKAGE_luci-app-usb3disable=y
-CONFIG_PACKAGE_luci-app-luci-app-netdata=y
-CONFIG_PACKAGE_luci-app-luci-app-rtbwmon=y
-
+################################################
+# 状态相关应用
+################################################
+# 实时信息插件
+CONFIG_PACKAGE_luci-app-rtbwmon=y
+CONFIG_PACKAGE_rtbwmon=y
+# luci-app-netdata可控制的实时监控
+CONFIG_PACKAGE_netdata=y
+CONFIG_PACKAGE_luci-app-netdata=y
+# 释放内存插件
+CONFIG_PACKAGE_luci-app-ramfree=y
+# 在线用户插件
+CONFIG_PACKAGE_luci-app-onliner=y
+###############################################
+# 服务相关应用
+###############################################
+# Wi-Fi 连接的时间表工具
+CONFIG_PACKAGE_luci-app-wifischedule=y
+###############################################
 # 存储相关应用
+###############################################
+# Go WebDAV 应用，提供 WebDAV 服务的 Go 语言实现。
 CONFIG_PACKAGE_luci-app-gowebdav=y
-# 其他
+# 文件助手
+CONFIG_PACKAGE_luci-app-fileassistant=y
+##############################################
+# 网络相关应用
+##############################################
+# 应用过滤插件
+CONFIG_PACKAGE_appfilter=y
+# Socat插件
 CONFIG_PACKAGE_luci-app-socat=y
+# IP限速
+CONFIG_PACKAGE_luci-app-eqos=y
 
 " >> .config
